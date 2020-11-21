b5009d043d4636f53b413c4f2e9863fc763a776f
mrshuhrat <shuhratadm1n@gmail.com>
2020-11-21 10:41:26 +0500

    Add catigories and select

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,14 @@ var normalizedMovies = movies.map(function (movie, i) {
   };
 }).slice(0, 300);
 
-var mocieCatigories = [];
+var select = ['Alphabet (A-Z)', 'Alphabet (Z-A)', 'Rating (1-10)', 'Rating (10-1)']
+var categories = [];
 
 var elMoviesWrapper = $_('.movies');
 var elSearchInput = $_('.search-input');
 var elSearchButton = $_('.search-button');
+var elCategoriesSelect = $_('.catigories-select');
+var elSortSelect = $_('.sort-selct');
 var elMoviesTemplate = $_('#movie_template').content;
 
 var createMoviesElement = function (movie) {
@@ -61,6 +64,52 @@ var searchMovie = function () {
 
   renderMovies(newResult);
 }
+
+
+const getCategories = function(normalizedMovies) {
+  categories.push(normalizedMovies[0].categories[0]);
+
+  normalizedMovies.forEach(function(movie) {
+    movie.categories.forEach(function(category) {
+      if (!(categories.includes(category))) {
+        categories.push(category);
+      }
+    });
+  });
+  return categories;
+};
+
+getCategories(normalizedMovies);
+
+
+for (var category of categories) {
+  var newOption = document.createElement('option');
+  newOption.textContent = category;
+  newOption.value = category;
+
+  elCategoriesSelect.appendChild(newOption);
+}
+var newOptionCategories = document.createElement('option');
+newOptionCategories.textContent = 'Categories';
+newOptionCategories.selected = true;
+newOptionCategories.disabled = true;
+
+elCategoriesSelect.appendChild(newOptionCategories);
+
+
+for (var sort of select) {
+  var newOption = document.createElement('option');
+  newOption.textContent = sort;
+  newOption.value = sort;
+
+  elSortSelect.appendChild(newOption)
+}
+var newOptionSelect = document.createElement('option');
+newOptionSelect.textContent = 'Select'
+newOptionSelect.selected = true;
+newOptionSelect.disabled = true;
+
+elSortSelect.appendChild(newOptionSelect);
 
 elSearchInput.addEventListener('input', searchMovie);
 elSearchButton.addEventListener('click', searchMovie);
